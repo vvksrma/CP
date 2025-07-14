b87c780416427fb5051e17a14b7ae93ebce81644
vvksrma <viveksharma0390@gmail.com>
2025-07-14 17:40:16 +0530

    Bitmasking

--- bitmanipulation.cpp
+++ bitmanipulation.cpp
@@ -1,6 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int getIthbit(int n, int i){
+	int mask = (1<<i);
+	return (n & mask) > 0 ? 1 : 0;
+}
+
+void setIthbit(int &n, int i){
+	int mask = (1<<i);
+	n = (n|mask);
+}
+
+void clearIthbit(int &n, int i){
+	int mask = ~(1<<i);
+	n = n & mask;
+}
+
 int main(){
 #ifndef ONLINE_JUDGE
 
@@ -8,15 +23,22 @@ int main(){
     freopen("output.txt", "w", stdout);
 
 #endif
-	int x;
-	cin >> x;
+	// int x;
+	// cin >> x;
 
-	if(x&1){
-		cout << "odd";
-	}else{
-		cout << "even";
-	}
+	// if(x&1){
+	// 	cout << "odd";
+	// }else{
+	// 	cout << "even";
+	// }
 
+	int n = 9; //001001
+	int i;
+	cin>>i;
+
+	clearIthbit(n, i);
+
+	cout << n;
 
 	return 0;
 }
